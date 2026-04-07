93a2520b07c3efc64f207ecf6f704e70597904f2
Edgar <edgarmaia.dev@gmail.com>
2026-04-07 10:51:50 -0300

    <feat> (Upd) add to Github.

--- src/services/auth/signin/post.ts
+++ src/services/auth/signin/post.ts
@@ -1,11 +1,22 @@
 export async function signin(data: unknown) {
-  const res = await fetch("http://localhost:3000/users/signin", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  try {
+    const res = await fetch("http://localhost:3333/users/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-  return res.json();
+    if (!res.ok) {
+      // Isso ajuda a saber se o erro é na API (ex: senha errada ou erro de banco)
+      const errorData = await res.json();
+      throw new Error(errorData.message || 'Erro na autenticação');
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Erro ao conectar com a API:", error);
+    throw error;
+  }
 }
